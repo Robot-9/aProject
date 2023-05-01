04c59b85dcad40bd45a063b7094196a5521c41f2
Robot-9 <mr.stepanyan.2006@mail.ru>
2023-05-01 13:41:42 +0300

    tab

--- src/main/java/app/Application.java
+++ src/main/java/app/Application.java
@@ -1,5 +1,6 @@
 package app;
 
+import controls.InputFactory;
 import controls.Label;
 import io.github.humbleui.jwm.*;
 import io.github.humbleui.jwm.skija.EventFrameSkija;
@@ -179,8 +180,8 @@ public class Application implements Consumer<Event> {
                             // завершаем обработку, иначе уже разрушенный контекст
                             // будет передан панелям
                             return;
-
                         }
+                        case TAB -> InputFactory.nextTab();
                     }
             }
         }
