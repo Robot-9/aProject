4c635301ab89110d3f6f99aa05a5a501a7a0eb2d
Robot-9 <mr.stepanyan.2006@mail.ru>
2023-05-01 14:28:45 +0300

    Control

--- src/main/java/panels/PanelControl.java
+++ src/main/java/panels/PanelControl.java
@@ -23,6 +23,10 @@ import static app.Colors.FIELD_TEXT_COLOR;
  */
 public class PanelControl extends GridPanel {
     /**
+     * Кнопка "решить"
+     */
+    private final Button solve;
+    /**
      * Кнопки
      */
     public List<Button> buttons;
@@ -119,6 +123,40 @@ public class PanelControl extends GridPanel {
                 PanelRendering.task.addRandomPoints(cntField.intValue());
         });
         buttons.add(addPoints);
+
+        // управление
+        Button load = new Button(
+                window, false, backgroundColor, PANEL_PADDING,
+                6, 7, 0, 5, 3, 1, "Загрузить",
+                true, true);
+        load.setOnClick(() -> {
+            PanelRendering.load();
+            cancelTask();
+        });
+        buttons.add(load);
+
+        Button save = new Button(
+                window, false, backgroundColor, PANEL_PADDING,
+                6, 7, 3, 5, 3, 1, "Сохранить",
+                true, true);
+        save.setOnClick(PanelRendering::save);
+        buttons.add(save);
+
+        Button clear = new Button(
+                window, false, backgroundColor, PANEL_PADDING,
+                6, 7, 0, 6, 3, 1, "Очистить",
+                true, true);
+        clear.setOnClick(() -> PanelRendering.task.clear());
+        buttons.add(clear);
+
+        solve = new Button(
+                window, false, backgroundColor, PANEL_PADDING,
+                6, 7, 3, 6, 3, 1, "Решить",
+                true, true);
+        solve.setOnClick(() -> {
+            PanelRendering.task.solve();
+        });
+        buttons.add(solve);
     }
 
     /**
@@ -201,5 +239,13 @@ public class PanelControl extends GridPanel {
         for (Label label : labels) {
             label.paint(canvas, windowCS);
         }
+    }
+    /**
+     * Сброс решения задачи
+     */
+    private void cancelTask() {
+        PanelRendering.task.cancel();
+        // Задаём новый текст кнопке решения
+        solve.text = "Решить";
     }
 }
